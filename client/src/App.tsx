5f2a2dedda6abff212141175b735bd667d06c927
Andrew Davis <atd285@gmail.com>
2020-02-06 21:00:43 -0500

    mapbox map rendered

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const App = () => {
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
+const App: React.FC = () => {
+  const [viewport, setViewport] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 2
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={setViewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+    />
   );
-}
+};
 
 export default App;
