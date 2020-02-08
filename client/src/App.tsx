4e4c8adf6f80768ece1bd7fbc50c9cf688d15af1
Andrew Davis <atd285@gmail.com>
2020-02-07 22:32:36 -0500

    map markers need debugging

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,83 @@
 import React, { useState, useEffect } from "react";
 import ReactMapGL, { Marker } from "react-map-gl";
-import MarkerData from "./components/Marker";
+
 const App: React.FC = () => {
   const [viewport, setViewport] = useState<{
     width: number | string;
     height: number | string;
-    latitude: any;
-    longitude: any;
+    latitude: number;
+    longitude: number;
+
     zoom: number;
   }>({
     width: window.innerWidth,
     height: window.innerHeight,
-    latitude: 37.7577,
-    longitude: -122.4376,
+    latitude: 31.9742044,
+    longitude: -49.25875,
     zoom: 2
   });
   const [locationData, setLocationData] = useState<any>();
-  const [loading, setLoading] = useState(true);
   console.log(locationData);
+  type CityProps = {
+    longitude: number;
+    latitude: number;
+    key: string;
+    title: string;
+  };
   useEffect(() => {
     async function fetchData() {
-      setLoading(true);
       let response = await fetch("http://localhost:5000/api/logs");
       let data = await response.json();
       setLocationData(data);
-      setLoading(false);
     }
     fetchData();
   }, []);
+  useEffect(() => {
+    const handleResize = () => {
+      setViewport({
+        ...viewport,
+        height: window.innerHeight,
+        width: window.innerWidth
+      });
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
   return (
     <ReactMapGL
       {...viewport}
-      onViewportChange={setViewport}
-      mapStyle="mapbox://styles/mapbox/dark-v10"
+      onViewportChange={viewport => {
+        // call `setState` and use the state to update the map.
+        setViewport(viewport);
+      }}
+      mapStyle="mapbox://styles/mapbox/light-v10"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
     >
-      <MarkerData data={locationData} />
+      {locationData === undefined ? (
+        <h1>HELLO</h1>
+      ) : (
+        locationData.map((city: any) => {
+          return (
+            <Marker
+              latitude={city.latitude}
+              longitude={city.longitude}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <span
+                role="img"
+                aria-label="map marker emoji"
+                style={{ fontSize: `${viewport.zoom * 0.2}rem` }}
+              >
+                📷
+              </span>
+            </Marker>
+          );
+        })
+      )}
     </ReactMapGL>
   );
 };
